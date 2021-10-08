1c61c34823166b2a6f3ad0053635f6fe5e55459a
mm96249624 <89981035+mm96249624@users.noreply.github.com>
2021-10-08 23:00:00 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/Im
+import turtle as t
+min_level = 60
+max_level - 100
+def read_pulse(hour):
+    "read pulse levels 3times for each hour"
+    pass
+
+def measure_pulse(r1, r2, r3):
+    "accepts 3pulses values for each hour then calculate and return their average"
+    if r1<0 or r2<0 or r3<0:
+        return "negative"
+    else:
+        average = (r1 + r2 +r3)/3
+        return average
+
+    def abnormal(avg1, avg2, avg3, avg4):
+        pass
+
+def warning_doctor(hour):
+    pass
+
+def draw_chart(avg1, avg2, avg3, avg4)
+    "draws column chart using turle to show pulse averages duringthe four hours."
+
+def main():
+    r1 = float(input("enter the first reading : "))
+    r2 = float(input("enter the second reading : "))
+    r3 = float(input("enter the third reading : "))
+    average = meausre_pulse(r1,r2,r3)
+    print("\nthe average is :",average)
+
+if __name__ == "__main__"
+    main()
+    
